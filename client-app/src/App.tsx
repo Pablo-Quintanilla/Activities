4576ebbb7675785e374b26481415df14a15c96ae
Your Name <you@example.com>
2022-08-15 13:25:11 -0500

    End of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header, Icon, List, ListItem } from 'semantic-ui-react';
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data)
+    })
+  }, []) // added an empty array of dependencies to prevent loops.
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+        {activities.map((activity: any) => (
+          <ListItem key={activity.id}>
+            {activity.title}
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
